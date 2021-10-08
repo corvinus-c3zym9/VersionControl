7dcae2701de2c72a056eadcbcab8567cf259106a
C3ZYM9 <reka.szabo5@stud.uni-corvinus.hu>
2021-10-08 11:35:47 +0200

    Format Table

--- WEEK04/WEEK04/Form1.cs
+++ WEEK04/WEEK04/Form1.cs
@@ -14,17 +14,20 @@ namespace WEEK04
 {
     public partial class Form1 : Form
     {
+        //private int _milion = (int)Math.Pow(10,6);
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
         Excel.Application xlApp;
         Excel.Workbook xlWB;
         Excel.Worksheet xlSheet;
+        string[] headers;
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
             CreateExcel();
+            FormTable();
         }
 
         private void LoadData()
@@ -40,7 +43,7 @@ namespace WEEK04
                 xlWB = xlApp.Workbooks.Add(Missing.Value);
                 xlSheet = xlWB.ActiveSheet;
 
-                //CreateTable();
+                CreateTable();
 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
@@ -55,6 +58,77 @@ namespace WEEK04
                 xlWB = null;
                 xlApp = null;
             }
+        }
+
+        private void CreateTable()
+        {
+            headers = new string[] {
+                    "Kód",
+                    "Eladó",
+                    "Oldal",
+                    "Kerület",
+                    "Lift",
+                    "Szobák száma",
+                    "Alapterület (m2)",
+                    "Ár (mFt)",
+                    "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (var Flat in Flats)
+            {
+                values[counter, 0] = Flat.Code;
+                values[counter, 1] = Flat.Vendor;
+                values[counter, 2] = Flat.Side;
+                values[counter, 3] = Flat.District;
+                values[counter, 4] = Flat.Elevator ? "van" : "nincs";
+                values[counter, 5] = Flat.NumberOfRooms;
+                values[counter, 6] = Flat.FloorArea;
+                values[counter, 7] = Flat.Price;
+                values[counter, 8] = "=(H2/G2)*1000000";
+                counter++;
+            }
+
+            var range = xlSheet.get_Range(
+                                GetCell(2, 1),
+                                GetCell(1 + values.GetLength(0), values.GetLength(1)));
+            range.Value2 = values;
+
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+        private void FormTable()
+        {
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
         }
     }
 }
