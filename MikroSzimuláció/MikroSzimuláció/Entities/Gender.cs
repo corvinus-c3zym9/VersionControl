7bf2dcd9ef14fc09da2206e74c7d83f818f32ff9
C3ZYM9 <reka.szabo5@stud.uni-corvinus.hu>
2021-12-03 10:02:07 +0100

    osztályok létrehozása

---
+++ MikroSzimuláció/MikroSzimuláció/Entities/Gender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikroSzimuláció.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+    //mostantól férfi érték = Gender.Male
+    //női nem --> Gender évaNeme = (Gender)2 (átkasztoljuk a 2-es megadott értéket
+    // + egyszerűbb if --> if(évaNeme == Gender.Female)
+}
