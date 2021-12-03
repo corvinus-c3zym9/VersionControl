7a4c841d983774d39dc1c2facba8511736e6205d
C3ZYM9 <reka.szabo5@stud.uni-corvinus.hu>
2021-12-03 10:47:52 +0100

    Szimuláció

--- MikroSzimuláció/MikroSzimuláció/Form1.cs
+++ MikroSzimuláció/MikroSzimuláció/Form1.cs
@@ -14,6 +14,8 @@ namespace MikroSzimuláció
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234); //1234-től generáljon véletlenszámokat
+
         List<Person> Population = null;
         List<BirthProbability> BirthProbabilities = null;
         List<DeathProbability> DeathProbabilities = null;
@@ -25,6 +27,26 @@ namespace MikroSzimuláció
             Population = GetPopulation(@"C:\Users\User\Desktop\BCE\5\IRF\Temp\nép-teszt.csv"); //C:\Users\User\Desktop\BCE\5\IRF\Temp
             BirthProbabilities = GetBirthProbabilities(@"C:\Users\User\Desktop\BCE\5\IRF\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Users\User\Desktop\BCE\5\IRF\Temp\halál.csv");
+            
+            
+            // Végigmegyünk a vizsgált éveken
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         public List<Person> GetPopulation(string csvpath) //létrehozunk egy változót
@@ -49,6 +71,7 @@ namespace MikroSzimuláció
             return population; //visszaadjuk a belső függvény értékét
         }
 
+        //lemásoljuk a GetPopulation függvényt
         public List<BirthProbability> GetBirthProbabilities(string csvpath) //létrehozunk egy változót
         {
             List<BirthProbability> population = new List<BirthProbability>(); //létrehozunk egy függvényt
@@ -71,6 +94,7 @@ namespace MikroSzimuláció
             return population; //visszaadjuk a belső függvény értékét
         }
 
+        //lemásoljuk a GetPopulation függvényt
         public List<DeathProbability> GetDeathProbabilities(string csvpath) //létrehozunk egy változót
         {
             List<DeathProbability> population = new List<DeathProbability>(); //létrehozunk egy függvényt
@@ -92,5 +116,7 @@ namespace MikroSzimuláció
 
             return population; //visszaadjuk a belső függvény értékét
         }
+
+        
     }
 }
